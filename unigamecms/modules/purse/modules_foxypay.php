6a4988609430b24d2245bc6eb7fd11db699d5035
Stanislav <twileck1@hotmail.com>
2025-02-03 22:13:55 +0200

    Add files via upload

--- unigamecms/modules/purse/modules_foxypay.php
+++ unigamecms/modules/purse/modules_foxypay.php
@@ -5,6 +5,7 @@ if (isset($_GET['foxypay']) && $_GET['foxypay'] === 'pay') {
 	$payMethod = 'foxypay'; 												// Устанавливаем метод оплаты
 	$currentCurrency = Payments::getCashierCurrency($payMethod); 			// Получаем текущую валюту кассира
 
+
 	try {
 		$source = file_get_contents('php://input');
 		$requestData = json_decode($source, true);
@@ -23,13 +24,23 @@ if (isset($_GET['foxypay']) && $_GET['foxypay'] === 'pay') {
 		if (strtoupper($requestData["sign"]) !== strtoupper($signature)) {
 			throw new Exception('Invalid signature');
 		}
-
 		$siteCurrency = sys()->currency()->code;
-		$amountInCents = $requestData["amount"];    // Сумма, которая пришла
+		$amountInCents = $requestData["amount"];    // Сумма котора пришла
 		$amountCurrency = $requestData["currency"];  // Код валюты
 
-		// Конвертация в валюту сайта через FoxypayConverter
-		$amountInSiteCurrency = (new FoxypayConverter($siteCurrency))->convertCurrency($amountInCents, $amountCurrency, $siteCurrency);
+		if ($siteCurrency != "RUB") {
+			$amountInSiteCurrency = (new FoxypayConverter($siteCurrency))->convertCurrency($amountInCents, $siteCurrency, $amountCurrency);
+		} else {
+			if ($requestData["currency"] == "UAH") {
+				$convertCurrency = (new CurrencyConverter())->getCurrencyRUB("UAH", 2);
+				$convert = $requestData["amount"] * $convertCurrency / 1000;
+			} else {
+				$convertCurrency = (new CurrencyConverter())->getCurrencyRUB($requestData["currency"], 0);
+				$convert = $requestData["amount"] / 100;
+				$convert = $convert * $convertCurrency;
+			}
+			$amountInSiteCurrency = round($convert);
+		}
         
 		$amount = clean($amountInSiteCurrency, 'float');
 		$payNumber = clean($requestData["code"], 'varchar');
@@ -44,7 +55,7 @@ if (isset($_GET['foxypay']) && $_GET['foxypay'] === 'pay') {
 				exit('Old');
 			}
 			// Выполняем действия по обработке платежа
-			$Pm->doPayAction($pdo, $userInfo, $amount, $conf->bank, $payMethod, $payNumber, $messages[$siteCurrency]);
+			$Pm->doPayAction($pdo, $userInfo, $amount, $conf->bank, $payMethod, $payNumber, $messages['RUB']);
 			exit('OK');
 		}
 	} catch (Exception $e) {
